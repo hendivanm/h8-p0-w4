6e54a425b9748b34814952ab54f2785d994555e5
Hendi Ivan Mahendro <hendivanm.him@gmail.com>
2018-06-07 15:38:32 +0700

    Ubah Huruf

---
+++ exercise-5.js
@@ -0,0 +1,11 @@
+function ubahHuruf(kata) {
+    return kata.replace (/[a-zA-Z]/g, function(i) {
+      return String.fromCharCode (i.charCodeAt(0)+1);
+    });
+}
+
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
